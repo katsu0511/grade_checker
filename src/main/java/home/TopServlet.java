a76e869b31d7c92a41a0726890b98a14a1435e52
Katsuya <haradakatsuya190511@gmail.com>
2024-12-09 13:55:19 -0800

    トップページを追加

---
+++ src/main/java/home/TopServlet.java
@@ -0,0 +1,33 @@
+package home;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TopServlet
+ */
+@WebServlet("/top")
+public class TopServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TopServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/app/home/top.jsp").forward(request, response);
+	}
+}
