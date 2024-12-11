9d3eda112e8dd9f41e1da71e634539963f20e1f1
Katsuya <haradakatsuya190511@gmail.com>
2024-12-11 01:33:05 -0800

    ログアウト機能を追加

---
+++ src/main/java/home/LogoutServlet.java
@@ -0,0 +1,44 @@
+package home;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		final HttpSession SESSION = request.getSession();
+		final String USER_ID = (String) SESSION.getAttribute("user_id");
+		final String PASSWORD = (String) SESSION.getAttribute("password");
+
+		if (USER_ID != null && PASSWORD != null) {
+			SESSION.invalidate();
+			response.sendRedirect(request.getContextPath() + "/login");
+		} else {
+			response.sendRedirect(request.getContextPath() + "/login");
+		}
+	}
+}
