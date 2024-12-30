9d1a724328383e6207be08b767cab0ba7b21643f
Katsuya <haradakatsuya190511@gmail.com>
2024-12-30 15:32:59 -0800

    add graph servlet

---
+++ src/main/java/graph/ShowGradeGraphServlet.java
@@ -0,0 +1,34 @@
+package graph;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowGradeGraphServlet
+ */
+@WebServlet("/graph/grade")
+public class ShowGradeGraphServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowGradeGraphServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
