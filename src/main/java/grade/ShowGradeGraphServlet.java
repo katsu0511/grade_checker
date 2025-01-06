886d1324a79bcae227c97f0574dc295a7d5e8e11
Katsuya <haradakatsuya190511@gmail.com>
2025-01-06 13:01:43 -0800

    modified css

--- src/main/java/grade/ShowGradeGraphServlet.java
+++ src/main/java/grade/ShowGradeGraphServlet.java
@@ -51,10 +51,8 @@ public class ShowGradeGraphServlet extends HttpServlet {
 			Connection conn = null;
 			PreparedStatement pstmt1 = null;
 			PreparedStatement pstmt2 = null;
-			PreparedStatement pstmt3 = null;
 			ResultSet rset1 = null;
 			ResultSet rset2 = null;
-			ResultSet rset3 = null;
 			
 			try {
 				conn = USER_DAO.getConnection();
@@ -90,21 +88,8 @@ public class ShowGradeGraphServlet extends HttpServlet {
 					TERMS.add(rset2.getString(1));
 				}
 				
-				
-				String sql3 = "SELECT     Mark  "
-							+ "FROM       Mark  "
-							+ "ORDER BY   MarkID";
-				pstmt3 = conn.prepareStatement(sql3);
-				rset3 = pstmt3.executeQuery();
-				final ArrayList<String> MARKS = new ArrayList<String>();
-				
-				while (rset3.next()) {
-					MARKS.add(rset3.getString(1));
-				}
-				
 				request.setAttribute("grades", GRADES);
 				request.setAttribute("terms", TERMS);
-				request.setAttribute("marks", MARKS);
 				
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -112,10 +97,8 @@ public class ShowGradeGraphServlet extends HttpServlet {
 				try {
 					pstmt1.close();
 					pstmt2.close();
-					pstmt3.close();
 					rset1.close();
 					rset2.close();
-					rset3.close();
 					conn.close();
 				} catch (SQLException e) { }
 			}
